c5aded061bae9a3f22b9e919b5e75ab5cb3d8ce7
Hindu <hindukoppala2006@gmail.com>
2026-07-10 17:05:20 +0530

    first commit

---
+++ tsDemo.ts
@@ -0,0 +1,9 @@
+// Simple Types : Boolean, String, number
+
+let age : number = 20;
+let Name : string = "Alice";
+let isStudent : boolean = true;
+
+console.log(`${Name}`);
+console.log(`${age}`);
+console.log(`IsStudent : ${isStudent}`);
